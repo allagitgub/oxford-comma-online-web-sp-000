95fc52cf2638c0aec9e42237c30b787c5762343e
Alla Kuzminova <alchik@gmail.com>
2020-09-09 01:17:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if(array.length == 1)
     array.to_s
   elsif (array.length == 2)
-    array[array.length - 1] = " and " << array[array.length - 1]
+    array[array.length - 1] = " and " << array[array.length - 1].join
   else
     array[array.length - 1] = "and " << array[array.length - 1]
     array.join(",  ")
