b1e6d052a4627e642850725d90337573f2190ada
Alla Kuzminova <alchik@gmail.com>
2020-09-09 01:12:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,4 @@
 def oxford_comma(array)
-  and_str = "and "
-  array[array.length - 1] = ant_str << array[array.length - 1]
+  array[array.length - 1] = "and " << array[array.length - 1]
   array.join(",  ")
 end
